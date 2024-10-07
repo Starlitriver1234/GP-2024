07e64e612f5976e55256e9d9b6634e6f677b84e4
Starlitriver1234 <C24745999@mytudublin.ie>
2024-10-07 16:55:22 +0100

    hhh

---
+++ test-game/sprite_2d.gd
@@ -0,0 +1,32 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+@export var speed:float = 1
+@export var rot_speed:float = 100
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	# rotate(deg_to_rad(rot_speed * delta))
+	# translate(Vector2(0, speed * delta),)
+	
+	# global_translate(Vector2(0, speed * delta))
+	
+	var s:float = 150
+	var rs:float = 150
+	if (Input.is_key_pressed(KEY_SHIFT)):
+		s = s * 5
+		rs = rs * 2
+	
+	if Input.is_key_pressed(KEY_UP):
+		translate(Vector2(0, - s * delta))
+	if Input.is_key_pressed(KEY_DOWN):
+		translate(Vector2(0, s * delta))
+	if Input.is_key_pressed(KEY_RIGHT):
+		translate(Vector2(rs * delta, 0))
+	if Input.is_key_pressed(KEY_LEFT):
+		translate(Vector2(-rs * delta, 0))
+	pass
