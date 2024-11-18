953ba58925a3d5f7970e88752c4c951196d0cfc0
Starlitriver1234 <C24745999@mytudublin.ie>
2024-11-18 15:15:10 +0000

    Commit 3

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -8,6 +8,13 @@ func _draw() -> void:
 	r.size.x = r.size.x / 2
 	draw_rect(Rect2(r), Color.RED, true, 10, true)
 	var p = get_viewport().get_mouse_position()
+	print(p)
+	if p.x < h;
+		draw_react(Rect2(0, 0, h, r.size.y), Color.RED, true)
+		draw_react(Rect2(0, 0, h, r.size.y), Color.BLUE, true)
+	else:
+		draw_react(Rect(0, 0, h, r.size.y), Color.BLUE, true, 10, true)
+		draw_react(Rect(0, 0, h, r.size.y), Color.YELLOW, true, 10, true)
 	if p.x < 10:
 		print()
 	elif p < 20:
