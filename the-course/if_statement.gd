5781f8998b26b68a9ce98e2092477f6c7c0b1470
Starlitriver1234 <C24745999@mytudublin.ie>
2024-12-06 15:52:27 +0000

    Commit 2

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -24,17 +24,7 @@ func _draw() -> void:
 		draw_rect(Rect2(hw, 0, hw, hh), Color.RED, true, 10, true)
 	else:
 		draw_rect(Rect2(hw, hh, hw, hh), Color.RED, true, 10, true)
-	
 
-		==
-	 <
-	 >
-	 <=
-	 >=
-	 && and
-	 || or
-	 not
-	
 	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
