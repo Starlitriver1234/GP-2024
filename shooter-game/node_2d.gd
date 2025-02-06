c9bd09f0cbc40ec2e2f7d4ee1da0aeb6c63a62d1
Starlitriver1234 <C24745999@mytudublin.ie>
2025-02-06 12:39:17 +0000

    Commit 2

--- shooter-game/node_2d.gd
+++ shooter-game/node_2d.gd
@@ -6,16 +6,16 @@ var count: int
 func _draw() -> void:
 	var l:float = $num_lines.value
 	for i in range(l):
-		draw_line(Vector2(i*100, 0), Vector2(i*100), 0)
+		draw_line(Vector2(i*100, 0), Vector2(i*100, 500), Color.BEIGE, 10)
 		
-		var theta_inc = PI * 2.0/l
-		for i in range(l):
-			var theta = (theta_inc) * i
-			var x = sin(theta) * radius
-			var y = cos(theta) * radius
-			draw_circle(Vector2(x, y), radius, Color.CO)
+	var theta_inc = PI * 2.0/l
+	for i in range(l):
+		var theta = (theta_inc) * i
+		var x = sin(theta) * radius
+		var y = cos(theta) * radius
+		draw_circle(Vector2(x, y), radius, Color.CORNSILK, false)
 		
-		pass
+	pass
 		
 func _process(delta: float) -> void:
 	queue_redraw()
